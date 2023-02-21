77b3f1764d9bc7bc9a15470007216945b13ef42a
Willian <wilsouza541@gmail.com>
2023-02-21 20:00:43 -0300

    Apos salvar codigo no Intellij

---
+++ primeiro programa Intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,25 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma { // (shift + F6)
+    public static void main(String[] args) { // digita "main" e enter
+        Gato gato = new Gato(); // caso o import não estiver sendo utilizado limpar com (Ctrl + Alt + o)
+
+        System.out.println(gato);
+
+        /*  (Ctrl + Shift + /) comenta toda a área selecionada
+            (Ctrl + /) comenta linha por linha
+        int a = 5;
+        int b = 3;
+        System.out.println("Hello World! " + (a+b)); //digita "sout" e enter*/
+    }
+}
+// (Ctrl + Shift + F10) para rodar o programa no terminal pela primeira vez
+// após terminal aberto apenas apertar (Shift + F10)
+// (Ctrl + y) apaga a linha
+// (Ctrl + d) duplica linha
+// (Ctrl + Shift + seta cima/baixo) move a linha
+// (Ctrl + Alt + l) Organiza/Indenta o código
+// (link de atalhos Intellij) http://www.basef.com.br/index.php/Atalhos_do_IntelliJ_Idea
+
